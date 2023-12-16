c831d4710afaaf83f239f0ca12870596f874c2a1
Denor74 <den74@inbox.ru>
2023-12-16 09:58:16 +0500

    bootstrap

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -10,16 +10,6 @@ use Message\Core\Route;
 
 // dirname(__DIR__, 1) ПУТЬ НА ПАПКУ НА ОДИН УРОВЕНЬ ВЫШЕ ТЕКУЩЕЙ
 require dirname(__DIR__, 1) . DIRECTORY_SEPARATOR . 'vendor'. DIRECTORY_SEPARATOR . 'autoload.php';
-// require __DIR__.'/vendor/autoload.php';
-
-//date_default_timezone_set — Устанавливает часовой пояс по умолчанию для всех функций даты/времени в скрипте
-//date_default_timezone_set('Asia/Yekaterinburg');
-
-/* explode — Разбивает строку с помощью разделителя
-Возвращает массив строк, полученных разбиением 
-строки string с использованием separator в качестве разделителя. */
-//php_uname — Возвращает информацию об операционной системе, на которой запущен PHP
-$os = explode(' ', php_uname())[0]; 
 
 
 /* Оператор разрешения области видимости (::)
